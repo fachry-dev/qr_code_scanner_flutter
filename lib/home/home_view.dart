c680e5b44aaac59c9cbdacc5c275df003a7a3d2f
fachry-dev <fai.rizky.w@gmail.com>
2026-01-27 08:23:50 +0700

    feat : membuat fitur untuk scan

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
+
 import 'package:qr_scanner/home/home_controller.dart';
 
 
+
+
+
 class HomeView extends StatelessWidget {
-  final HomeController controller = HomeController();
   
+
+  final HomeController controller = HomeController();
+
+ 
+
   HomeView({super.key});
 
+
+
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar:  AppBar(
+
         title: Text('EvenPass'),
+
         actions: [
+
           IconButton(
+
             onPressed: () => controller.goToQRCodeView(context),
+
             icon: Icon(Icons.qr_code_scanner)),
+
         ],
+
       ),
+
       body: Center(child: Text("Welcome to EvenPass"),),
+
     );
   }
-}
+  
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_scanner/home/home_view.dart';
+import 'package:qr_scanner/qr_code/qr_code_view.dart';
+// import 'package:qr_scanner/settings/setting_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,30 +10,28 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ScanGo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF43A078),
+          primary: const Color(0xFF43A078),
+        ),
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF1F8E9), 
       ),
-      home: HomeView(),
+      
+  
+      initialRoute: '/',
+      
+      routes: {
+        '/': (context) => HomeView(),
+        '/qr_scan': (context) => QrCodeView(),
+        // '/settings': (context) => const SettingView(),
+      },
     );
   }
 }
