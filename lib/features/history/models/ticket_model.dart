6799b39e842d7981212d7f2ecd75f7417b63b861
fachry-dev <fai.rizky.w@gmail.com>
2026-01-30 09:28:38 +0700

    fix : memperbaiki eror

--- lib/features/history/models/ticket_model.dart
+++ lib/features/history/models/ticket_model.dart
@@ -10,4 +10,13 @@ class TicketModel {
     required this.studentClass,
     required this.isRedeemed,
   });
+
+  factory TicketModel.fromJson(Map<String, dynamic> json) {
+    return TicketModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      studentClass: json['student_class'] as String,
+      isRedeemed: json['is_redeemed'] as bool,
+    );
+  }
 }
--- lib/qr_code/qr_code_service.dart
+++ lib/qr_code/qr_code_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:qr_scanner/features/history/models/ticket_model.dart';
 
 class QrCodeService {
   static final _baseUrl = 'https://event-ticketing-ruddy.vercel.app/';
@@ -8,5 +9,21 @@ class QrCodeService {
   Future<void> scanTicket(String qrData) async {
     await _dio.post('/api/scan', data: {'qr_data': qrData});
   }
+
+  Future<List<TicketModel>> getTickets() async {
+    final response = await _dio.get('/api/tickets');
+    return (response.data as List)
+        .map((ticketData) => TicketModel.fromJson(ticketData))
+        .toList();
+  }
+
+  Future<void> redeemTicket(String ticketId) async {
+    await _dio.post('/api/redeem', data: {'ticket_id': ticketId});
+  }
+
+  Future<void> cancelTicket(String ticketId) async {
+    await _dio.post('/api/cancel', data: {'ticket_id': ticketId});
+  }
+
 
 }
